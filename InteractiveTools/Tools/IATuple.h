b6143049dc1cc9f2ccbc3d3fa83ad6c65c1f93b6
Sebastian Westemeyer <sebastian.westemeyer@ort-online.net>
2017-03-09 12:05:30 +0100

    Added tuple and modified NSString extension

---
+++ InteractiveTools/Tools/IATuple.h
@@ -0,0 +1,18 @@
+//
+//  IATuple.h
+//  InteractiveTools
+//
+//  Created by Sebastian Westemeyer on 08.03.17.
+//  Copyright © 2017 ORT Interactive GmbH. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface IATuple<__covariant ObjectType1, __covariant ObjectType2> : NSObject<NSCopying>
+
+@property(nonatomic, strong) ObjectType1 first;
+@property(nonatomic, strong) ObjectType2 second;
+
+- (instancetype) initWithFirst:(ObjectType1) first second:(ObjectType2) second;
+
+@end
---
+++ InteractiveTools/Tools/IATuple.m
@@ -0,0 +1,59 @@
+//
+//  IATuple.m
+//  InteractiveTools
+//
+//  Created by Sebastian Westemeyer on 08.03.17.
+//  Copyright © 2017 ORT Interactive GmbH. All rights reserved.
+//
+
+#import "IATuple.h"
+
+@implementation IATuple
+
+- (instancetype) initWithFirst:(id) first second:(id) second {
+    self = [super init];
+    if (self != nil) {
+        self.first = first;
+        self.second = second;
+    }
+    return self;
+}
+
+- (BOOL)isEqualToTuple:(IATuple *)tuple {
+    if (!tuple) {
+        return NO;
+    }
+
+    BOOL haveEqualFirst = (!self.first && !tuple.first) || [self.first isEqual:tuple.first];
+    BOOL haveEqualSecond = (!self.second && !tuple.second) || [self.second isEqual:tuple.second];
+
+    return haveEqualFirst && haveEqualSecond;
+}
+
+#pragma mark - NSCopying
+
+- (id)copyWithZone:(NSZone *)zone {
+    IATuple *copy = [[[self class] allocWithZone:zone] init];
+    copy->_first = [self.first copyWithZone:zone];
+    copy->_second = [self.second copyWithZone:zone];
+    return copy;
+}
+
+#pragma mark - NSObject
+
+- (BOOL)isEqual:(id)object {
+    if (self == object) {
+        return YES;
+    }
+
+    if (![object isKindOfClass:[IATuple class]]) {
+        return NO;
+    }
+
+    return [self isEqualToTuple:(IATuple *)object];
+}
+
+- (NSUInteger)hash {
+    return [self.first hash] ^ [self.second hash];
+}
+@end
